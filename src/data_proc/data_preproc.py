434cbe3d489f26e6f7248c066777cf6e00aae051
jaketall7 <jaketall@icloud.com>
2026-03-04 18:28:27 -0800

    eurosat

--- src/data_proc/data_preproc.py
+++ src/data_proc/data_preproc.py
@@ -3,6 +3,14 @@ import torch
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
+#calcuated mean std
+
+mean_erosat = [0.31268863, 0.34511476, 0.37031967]
+std_eurosat = [0.19137047, 0.12704164, 0.10665024]
+
+mean_tny_imgnet = [0.4914, 0.4822, 0.4465]
+std_tny_imgnet = [0.2023, 0.1994, 0.2010]
+
 def get_mini_imagnet():
 
     tiny_imgnet_ds = load_dataset("zh-plus/tiny-imagenet", split="train")
@@ -15,11 +23,14 @@ def get_mini_imagnet():
 
     return tiny_imgnet_ds, tiny_imgnet_ds_val
 
+def get_eurosat_rgb():
+    eurosat_rgb_ds = load_dataset("blanchon/EuroSAT_RGB", split="train")
+    eurosat_rbg_ds_val = load_dataset("blanchon/EuroSAT_RGB", split="validation")
 
-def preproc_and_normalize_tiny_img_net(tiny_imgnet_ds, tiny_imgnet_ds_val) :
-    mean = [0.4914, 0.4822, 0.4465]  # calculated for this dataset
-    std = [0.2023, 0.1994, 0.2010]
+    return eurosat_rgb_ds, eurosat_rbg_ds_val
 
+
+def preproc_and_normalize_hf_ds(ds, val, mean, std) :
     transform = transforms.Compose([
         transforms.Lambda(lambda im: im.convert("RGB")),  # force grey scales to rgb
         transforms.Resize((224, 224)),
@@ -31,14 +42,14 @@ def preproc_and_normalize_tiny_img_net(tiny_imgnet_ds, tiny_imgnet_ds_val) :
         batch["image"] = [transform(im) for im in batch["image"]]
         return batch
 
-    tiny_imgnet_ds = tiny_imgnet_ds.with_transform(hf_transform)
-    tiny_imgnet_ds_val = tiny_imgnet_ds_val.with_transform(hf_transform)
+    ds = ds.with_transform(hf_transform)
+    val = val.with_transform(hf_transform)
 
-    return tiny_imgnet_ds, tiny_imgnet_ds_val
+    return ds, val
 
 def tiny_image_net_dataloaders():
     tiny_imgnet_ds, tiny_imgnet_ds_val = get_mini_imagnet()
-    tiny_imgnet_ds, tiny_imgnet_ds_val = preproc_and_normalize_tiny_img_net(tiny_imgnet_ds, tiny_imgnet_ds_val)
+    tiny_imgnet_ds, tiny_imgnet_ds_val = preproc_and_normalize_hf_ds(tiny_imgnet_ds, tiny_imgnet_ds_val,  mean_tny_imgnet, std_tny_imgnet)
 
     loader = DataLoader(
         tiny_imgnet_ds,
@@ -48,6 +59,24 @@ def tiny_image_net_dataloaders():
 
     loader_val = DataLoader(
         tiny_imgnet_ds_val,
+        batch_size=64,
+        shuffle=False
+    )
+
+    return loader, loader_val
+
+def eurosat_rgb_dataloaders():
+    eurosat_rgb_ds, eurosat_rgb_val = get_eurosat_rgb()
+    eurosat_rgb_ds, eurosat_rgb_val = preproc_and_normalize_hf_ds(eurosat_rgb_ds, eurosat_rgb_val, mean_erosat, std_eurosat)
+
+    loader = DataLoader(
+        eurosat_rgb_ds,
+        batch_size=64,
+        shuffle=False
+    )
+
+    loader_val = DataLoader(
+        eurosat_rgb_val,
         batch_size=64,
         shuffle=False
     )
