fd04ef0dca9fd0ba8b5e1f8a325c056c11f1e594
jaketall7 <jaketall@icloud.com>
2026-03-05 10:55:23 -0800

    merced

--- src/data_proc/data_preproc.py
+++ src/data_proc/data_preproc.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 import torch
 from torchvision import transforms
 from torch.utils.data import DataLoader
-
+from datasets import DatasetDict
 #calcuated mean std
 
 mean_erosat = [0.31268863, 0.34511476, 0.37031967]
@@ -11,8 +11,10 @@ std_eurosat = [0.19137047, 0.12704164, 0.10665024]
 mean_tny_imgnet = [0.4914, 0.4822, 0.4465]
 std_tny_imgnet = [0.2023, 0.1994, 0.2010]
 
-def get_mini_imagnet():
+mean_merced = [0.48421312, 0.49003859, 0.45048148]
+std_merced = [0.2180214, 0.20207059, 0.19583832]
 
+def get_mini_imagnet():
     tiny_imgnet_ds = load_dataset("zh-plus/tiny-imagenet", split="train")
     tiny_imgnet_ds_val = load_dataset("zh-plus/tiny-imagenet", split="valid")
 
@@ -29,6 +31,21 @@ def get_eurosat_rgb():
 
     return eurosat_rgb_ds, eurosat_rbg_ds_val
 
+def make_train_val_split(dataset_dict, val_size=0.2, seed=42):
+    split = dataset_dict["train"].train_test_split(
+        test_size=val_size,
+        seed=seed
+    )
+
+    return DatasetDict({
+        "train": split["train"],
+        "validation": split["test"]
+    })
+
+def get_uc_merced():
+    UC_Merced = load_dataset("blanchon/UC_Merced")
+    UC_Merced_train_val = make_train_val_split(UC_Merced)
+    return UC_Merced_train_val["train"], UC_Merced_train_val["validation"]
 
 def preproc_and_normalize_hf_ds(ds, val, mean, std) :
     transform = transforms.Compose([
@@ -47,36 +64,63 @@ def preproc_and_normalize_hf_ds(ds, val, mean, std) :
 
     return ds, val
 
-def tiny_image_net_dataloaders():
-    tiny_imgnet_ds, tiny_imgnet_ds_val = get_mini_imagnet()
-    tiny_imgnet_ds, tiny_imgnet_ds_val = preproc_and_normalize_hf_ds(tiny_imgnet_ds, tiny_imgnet_ds_val,  mean_tny_imgnet, std_tny_imgnet)
+# def tiny_image_net_dataloaders():
+#     tiny_imgnet_ds, tiny_imgnet_ds_val = get_mini_imagnet()
+#     tiny_imgnet_ds, tiny_imgnet_ds_val = preproc_and_normalize_hf_ds(tiny_imgnet_ds, tiny_imgnet_ds_val,  mean_tny_imgnet, std_tny_imgnet)
+#
+#     loader = DataLoader(
+#         tiny_imgnet_ds,
+#         batch_size=64,
+#         shuffle=False
+#     )
+#
+#     loader_val = DataLoader(
+#         tiny_imgnet_ds_val,
+#         batch_size=64,
+#         shuffle=False
+#     )
+#
+#     return loader, loader_val
+
+# def eurosat_rgb_dataloaders():
+#     eurosat_rgb_ds, eurosat_rgb_val = get_eurosat_rgb()
+#     eurosat_rgb_ds, eurosat_rgb_val = preproc_and_normalize_hf_ds(eurosat_rgb_ds, eurosat_rgb_val, mean_erosat, std_eurosat)
+#
+#     loader = DataLoader(
+#         eurosat_rgb_ds,
+#         batch_size=64,
+#         shuffle=False
+#     )
+#
+#     loader_val = DataLoader(
+#         eurosat_rgb_val,
+#         batch_size=64,
+#         shuffle=False
+#     )
+#
+#     return loader, loader_val
+
+def get_dataloaders(data = "tiny_imagenet"):
+
+    if data == "tiny_imagenet":
+        ds, ds_val = get_mini_imagnet()
+
+    elif data == "eurosat_rgb":
+        ds, ds_val = get_eurosat_rgb()
+
+    elif data == "uc_merced":
+        ds, ds_val = get_uc_merced()
+
+    ds, ds_val = preproc_and_normalize_hf_ds(ds, ds_val, mean_tny_imgnet, std_tny_imgnet)
 
     loader = DataLoader(
-        tiny_imgnet_ds,
+        ds,
         batch_size=64,
         shuffle=False
     )
 
     loader_val = DataLoader(
-        tiny_imgnet_ds_val,
-        batch_size=64,
-        shuffle=False
-    )
-
-    return loader, loader_val
-
-def eurosat_rgb_dataloaders():
-    eurosat_rgb_ds, eurosat_rgb_val = get_eurosat_rgb()
-    eurosat_rgb_ds, eurosat_rgb_val = preproc_and_normalize_hf_ds(eurosat_rgb_ds, eurosat_rgb_val, mean_erosat, std_eurosat)
-
-    loader = DataLoader(
-        eurosat_rgb_ds,
-        batch_size=64,
-        shuffle=False
-    )
-
-    loader_val = DataLoader(
-        eurosat_rgb_val,
+        ds_val,
         batch_size=64,
         shuffle=False
     )
